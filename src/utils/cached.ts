5b79a72a20a5cf4afeb92b0acad0d28dc694ef12
Armen <mench.harutyunyan@gmail.com>
2017-09-15 17:26:24 +0400

    bug fix

--- src/utils/cached.ts
+++ src/utils/cached.ts
@@ -2,6 +2,8 @@ export function Cached(target:any,key:string,desc:any){
     let initializer = desc.get;
     desc.get = function(){
         return Object.defineProperty(this,key,{
+            enumerable:true,
+            configurable:true,
             value:initializer.call(this)
         })[key];
     };
